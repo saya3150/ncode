4f7d23dabf945615038ae8a2e5f8150d3eab404f
saya <azu.partner@gmail.com>
2017-10-28 11:04:42 +0900

    [modify]database connect

--- view.php
+++ view.php
@@ -11,7 +11,24 @@
 		header("Location:./index.php");
 		exit();	
 	}
+try{
+	$db=new SQLite3('./'.$ncode.'.sqlite3');
+} catch (Exception $ex) {
+	print $e->getTraceAsString();
+	exit();
 
+}
+try{
+	$meta_db=new SQLite3('./meta.sqlite3');
+} catch (Exception $ex) {
+	print $e->getTraceAsString();
+	exit();
+
+}
+$result=$meta_db->query("select * from meta where ncodeid='".$ncode."'");
+$row=$result->fetchArray();
+$maintitle=$row["maintitle"];
+$result=$db->query("select * from data");
 ?>
 <html>
 <head>
