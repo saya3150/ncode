9b58a55efac1ceba9e02c24840897311608d41c2
saya <azu.partner@gmail.com>
2017-11-01 14:49:38 +0900

    [modify]データベース接続系の調整

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <?php
+    if(!empty($_POST["ncode"])){
+        $ncode=$_POST["ncode"];
+        try{
+            $db=new SQLite3('./meta.sqlite3');
+        } catch (Exception $ex) {
+            print $e->getTraceAsString();
+            exit();
+
+        }
+        $result=$db->query("select * from meta where ncode='".$ncode."'");
+        $row=$result->fetchArray();
+        $maintitle=$row["maintitle"];
+        }
 ?>
+
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 <title>ncode</title>
 </head>
 <body>
+    <?php
+        if(!empty($ncode)){
+            echo "<h1>".$maintitle."</h1>";
+        }else{
+            
+        }
+    ?>
 <h1></h1>
 </body>
 </html>
