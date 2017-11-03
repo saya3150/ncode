7a011dc689c3ba9465332bf4d89befc121b52ff2
saya <azu.partner@gmail.com>
2017-11-03 10:25:58 +0900

    [update]

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <?php
-    if(!empty($_GET["ncode"])){
-        $ncode=$_GET["ncode"];
         try{
             $db=new SQLite3('./meta.sqlite3');
         } catch (Exception $ex) {
             print $e->getTraceAsString();
             exit();
-
         }
+if(!empty($_GET["ncode"])){
+        $ncode=$_GET["ncode"];
         $result=$db->query("select * from meta where ncode='".$ncode."'");
         $row=$result->fetchArray();
         $maintitle=$row["maintitle"];
+        try{
+            $ncode_db=new SQLite3("./".$ncode.".sqlite3");
+        } catch (Exception $ex) {
+            echo "そのような小説は存在しません";
+            exit();
         }
+        $result=$ncode_db->query("select * from data");
+    }else{
+        $result=$db->query("select * from meta");
+    }
+    
 ?>
 
 <html>
@@ -24,7 +33,11 @@
         if(!empty($ncode)){
             echo "<h1>".$maintitle."</h1>";
         }else{
-            
+            echo "読みたい小説を選択してください.";
+            echo "<table border><tr><td>タイトル</td><td>選択</td></tr>";
+            while($row=$result->fetchArray()){
+                echo "<tr><td>".$row["maintitle"]."</td><td><a href='./index.php?ncode=".$row["ncode"]."'>選択</td></tr>";
+            }
         }
     ?>
 <h1></h1>
