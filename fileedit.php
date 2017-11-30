70ff406b989aabf4d9b36bfdc1cfaeb77b615fdb
saya <azu.partner@gmail.com>
2017-11-30 21:19:06 +0900

    [add]

--- fileedit.php
+++ fileedit.php
@@ -26,10 +26,9 @@ if($fp){
 	while ($line = fgets($fp)){
 		if(strpos($line,'<p class="novel_subtitle">') !== false){
 		      $state=1;
-                      $line=substr($line,27);
+                      $line=substr($line,26);
                       $line=substr($line,0,strpos($line,"</p>"));
-                      echo $line;
-			//$db->exec('insert into data values('.$atai.',"'.$line.'")');
+			$db->exec('insert into data values('.$atai.',"'.$line.'")');
                 }
 		if(strpos($line,'<div class="novel_bn">') !== false)$state=0;
 		if($state==1){
